c7a4d8ce9c2d5ac3b143b1788372a0fe968094d3
Kranthi Chaithanya <kranthi@qa05.dev.tumri.net>
2014-08-06 02:10:02 -0700

    Change to program to look more clean

--- DbbackupBasedOnDate.java
+++ DbbackupBasedOnDate.java
@@ -1,4 +1,3 @@
-package com.collective.qa.NetworkLatency.utils;
 
 import java.io.File;
 import java.text.DateFormat;
@@ -8,14 +7,12 @@ import java.util.Date;
 
 public class DbbackupBasedOnDate {
 
-	public static boolean dBBackup(String dbName, String dbUserName,
-			String dbPassword, String path) {
-
-		String executeCmd = "mysqldump -u " + dbUserName + " -p" + dbPassword + " --add-drop-database -B " + dbName + " -r " + path;
+	public static boolean executeCommad(String command) {
 		Process runtimeProcess;
 		try {
-			System.out.println(executeCmd);// this out put works in mysql shell
-			runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+			// System.out.println(executeCmd);// this out put works in mysql
+			// shell
+			runtimeProcess = Runtime.getRuntime().exec(command);
 			int processComplete = runtimeProcess.waitFor();
 
 			if (processComplete == 0) {
@@ -24,46 +21,84 @@ public class DbbackupBasedOnDate {
 			} else {
 				System.out.println("Could not create the backup");
 			}
+
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		return false;
 	}
 
-	public static void flagCheck(String filepath) {
-		DateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
-		File file = new File(filepath);
-		if(file.exists()){
-		Long lastModified = file.lastModified();
-		Date date = new Date(lastModified);
-		String output1 = dateformat.format(date).toString();
-		// System.out.println("File created date is "+output1);
+	public static boolean commands(String path) {
+		String dbName = "testlink";
+		String dbUserName = "root";
+		String dbPassword = "root";
 
-		try {
-			Date today = new Date();
-			String output = dateformat.format(today).toString();
-			// System.out.println("Current date is "+output);
-			if (output1.equals(output)) {
-				System.out
-						.println("As the file creation  & Current dates are equal. DB-Backup & other commands initiation aborted..");
-			} else {
-				System.out
-						.println("File Creation & Current dates differ. Initiating DB-Backup & other commands....");
-//				 DbbackupBasedOnDate.dBBackup("root","root","testlink","C:/Users/Master/Downloads/testlink_"+ output + ".sql");
+		String databaseCmd = "mysqldump -u " + dbUserName + " -p" + dbPassword + " --add-drop-database -B " + dbName + " -r " + path;
+		String pullCmd = "git pull" + path;
+		String addCmd = "git add" + path;
+		String cmtCmd = "git commit -m 'Latest Dump of testlink'";
+		String pushCmd = "git push" + path;
+		executeCommad(databaseCmd);
+		// executeCommad(pullCmd);
+		// executeCommad(addCmd);
+		// executeCommad(cmtCmd);
+		// executeCommad(pushCmd);
+		return false;
+	}
+
+	public static void flagCheck(String path) {
+
+		long createTime = 0;
+		File latestFile = null;
+		DateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+		File folder = new File(path);
+		if (folder.isDirectory()) {
+			for (File f : folder.listFiles()) {
+				if (createTime < f.lastModified()) {
+					latestFile = f;
+					createTime = f.lastModified();
+				}
+
 			}
-		} catch (Exception e) {
-			e.getMessage();
 		}
+		File file = new File(latestFile.getAbsolutePath());
+		System.out.println(latestFile.getAbsolutePath());
+		if (file.exists()) {
+			Long lastModified = file.lastModified();
+			Date date = new Date(lastModified);
+			String output1 = dateformat.format(date).toString();
+
+			 System.out.println("File created date is "+output1);
+
+			try {
+
+				Date today = new Date();
+				String output = dateformat.format(today).toString();
+				System.out.println("Current date is " + output);
+
+				if (output1.equals(output)) {
+					System.out
+							.println("As the file creation  & Current dates are equal. DB-Backup & other commands initiation aborted..");
+				} else {
+					System.out
+							.println("File Creation & Current dates differ. Initiating DB-Backup & other commands....");
+					DbbackupBasedOnDate
+							.commands("/home/kranthi/Dbbackup/testlink_"+ output + ".sql");
+
+				}
+
+			} catch (Exception e) {
+				e.getMessage();
+			}
 		} else {
 			System.out.println("File doesn't exist in the specified path");
 		}
-		
 
 	}
 
 	public static void main(String[] args) throws ParseException {
 
-		flagCheck("/Users/Collective/name.txt");
+		flagCheck("/home/kranthi/Dbbackup/");
 
 	}
 }
